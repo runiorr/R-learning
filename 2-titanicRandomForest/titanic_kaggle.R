441e8e26d0765f23922bfad093e3572431a02609
José Rodrigues <69124149+runiorr@users.noreply.github.com>
2021-12-31 09:09:20 -0300

    Update titanic_kaggle.R

--- 2-titanicRandomForest/titanic_kaggle.R
+++ 2-titanicRandomForest/titanic_kaggle.R
@@ -1,35 +1,35 @@
-#Chamando os pacotes que irei utilizar
+#Loading packages ('randomForest' is the machine learning model and 'ggplot2' is for charts) 
 library(randomForest)
 library(ggplot2)
 
-#Importar os dados
+#Importing the data
 train_set <- read.csv("train.csv", stringsAsFactors = T)
 test_set <-  read.csv("test.csv", stringsAsFactors = T)
 
-#Check dados faltantes
+#Check missing data
 colSums(is.na(train_set))
 colSums(is.na(test_set))
 colSums(train_set == "")
 colSums(test_set == "")
 
-#Criar coluna faltante no test_set
+#Create missing column in test_set
 test_set$Survived <- NA
 
-#Criando uma coluna para identificar se o dado é treino ou teste
+#Create a column to identify whether the data is training or testing data (both data frames must have the same columns/variables/features)
 train_set$IsTrainSet <- T
 test_set$IsTrainSet <- F
 
-#Agrupar os datasets usando rbind
+#Group the data frames using rbind
 titanic_set <- rbind(train_set, test_set)
 
-#Descritiva do conjunto
+#Summary of the new data frame
 summary(titanic_set)
 
-#Check dados faltantes
+#Check missing data again (now on the new dataframe)
 colSums(is.na(titanic_set))
 colSums(titanic_set == "")
 
-#Transformações simples ETL/4Cs (Cleaning, completing, correting, creating)
+#Simple transformations ETL/4Cs (Cleaning, completing, correting, creating)
 titanic_set$Survived <- as.factor(titanic_set$Survived)
 titanic_set$Pclass <- as.factor(titanic_set$Pclass)
 titanic_set$Age[is.na(titanic_set$Age)] <- median(titanic_set$Age, na.rm = T)
@@ -40,53 +40,53 @@ titanic_set$Embarked[titanic_set$Embarked==""] <- "S"
 titanic_set$Embarked <- as.factor(as.character(titanic_set$Embarked))
 table(titanic_set$Embarked)
 
-#Construir o modelo
+#Now we're going to build the model. Split intro train and test
 titanic_train <- titanic_set[titanic_set$IsTrainSet==T,]
 titanic_test  <- titanic_set[titanic_set$IsTrainSet==F,]
 
-#Criando a formula
+#Creating a formula
 survived_formula <- as.formula("Survived ~ Sex + Pclass + Age + SibSp + Parch + Fare + Embarked")
 
-#Criando o modelo
+#Building the model
 titanic_model <- randomForest(formula = survived_formula,
                               data = titanic_train,
                               ntree = 65,
                               importance = T)
 
-#Interpretando resultados
+#Interpreting results
 titanic_model
 
-#Mostrando graficos de curva
+#Show the error function of the model
 plot(titanic_model)
 
-#Gerando a matriz de importância das variáveis
+#Generating a variable importance matrix
 importance_var <-  importance(titanic_model, type=1)
 
 importance_var
   
-#Formatando a tabela
-tabela_importancia <- data.frame(variaveis=row.names(importance_var),
-                                 importancia=importance_var[,1]);tabela_importancia
+#Formatting the table
+importance_table <- data.frame(features=row.names(importance_var),
+                                 importance=importance_var[,1]);importance_table
 
-#Gerando o grafico
-grafico <- ggplot(tabela_importancia,
-                  aes(x=reorder(variaveis, importancia), y=importance_var)) +
+#Creating the graph
+model_graph <- ggplot(importance_table,
+                  aes(x=reorder(features, importance), y=importance_var)) +
                   geom_bar(stat = "identity", fill="#5cc9c1") +
                   coord_flip() +
                   theme_light(base_size = 20) +
                   xlab("") +
-                  ylab("Importância") +
-                  ggtitle("Importância das variáveis no Modelo RF\nAcurácia de 82.94%") +
+                  ylab("Importance") +
+                  ggtitle("Features importance in Random Forest model\nAccuracy of 82.94%") +
                   theme(plot.title = element_text(size = 18))
 
-grafico
+model_graph
 
-#Preparando material para submissão
+#Preparing material for submission
 
 submission <- data.frame(PassengerId = test_set$PassengerId,
                          Survived = predict(titanic_model, newdata = titanic_test))
 
-#View nos dados de saída
+#View the csv files and write it to a file for kaggle submission.
 View(submission)
 
 write.csv(submission, file = "titanic_kaggle.csv", row.names = F)
